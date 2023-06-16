a18f58794cb4f8ebc899bd5aed48b895429142a1
sheet11 <50392259+sheet11@users.noreply.github.com>
2023-06-16 08:00:10 +0700

    Update koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,6 +1,6 @@
 <?php
 
-$koneksi = mysqli_connect("localhost","root","","db_jadwal_cbt");
+$koneksi = mysqli_connect("localhost","phpmyadmin","P0ltekkes#","db_jadwal_cbt");
 
 if (mysqli_connect_error()){
     echo "koneksi database gagal : ".mysqli_connect_error;
